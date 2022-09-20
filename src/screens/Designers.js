5dc7381e10fa932f55f6e772df29bb8d9f27ed93
Koo Youngsill <koo1175@naver.com>
2022-09-21 00:09:51 +0900

    1. Designers 페이지 모바일2행, 데스크탑6행

--- src/screens/Designers.js
+++ src/screens/Designers.js
@@ -1,6 +1,52 @@
 import { collection, getDocs, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { dbService } from "../firebase";
+import styled from "styled-components";
+import { useMediaQuery } from "react-responsive";
+
+const Wrapper = styled.div`
+  left: 50%;
+  right: 50%;
+  margin: 0 auto;
+`;
+
+const MWrapper = styled.div`
+  margin: 0 auto;
+`;
+
+const Profile = styled.div`
+  margin: 0 auto;
+  margin-right: 48px;
+  width: 136px;
+  text-align: center;
+`;
+
+const Profiles = styled.div`
+  display: grid;
+  justify-content: center;
+  grid-template-columns: 200px 200px 200px 200px 200px 200px;
+`;
+
+const MProfile = styled.div`
+  margin: 0 auto;
+  width: 140px;
+  text-align: center;
+`;
+
+const MProfiles = styled.div`
+  display: grid;
+  justify-content: center;
+  grid-template-columns: 200px 200px;
+`;
+
+const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery({ minWidth: 992 });
+  return isDesktop ? children : null;
+};
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 991 });
+  return isMobile ? children : null;
+};
 
 const Designer = () => {
   const [designers, setDesigners] = useState([]);
@@ -20,14 +66,32 @@ const Designer = () => {
   }, []);
   console.log(designers);
   return (
-    <div>
-      {designers.map((designer) => (
-        <div key={designer.id}>
-          <img src={designer.profileUrl} alt="" />
-          <span>{designer.name}</span>
-        </div>
-      ))}
-    </div>
+  <>
+    <Desktop>
+      <Wrapper>
+        <Profiles>
+          {designers.map((designer) => (
+            <Profile key={designer.id}>
+              <img style={{ width: "140px", height: "140px" }} src={designer.profileUrl} alt="" />
+              <span>{designer.name}</span>
+            </Profile>
+          ))}
+        </Profiles>
+      </Wrapper>
+    </Desktop>
+    <Mobile>
+      <MWrapper>
+        <MProfiles>
+          {designers.map((designer) => (
+            <MProfile key={designer.id}>
+              <img style={{ width: "140px", height: "140px" }} src={designer.profileUrl} alt="" />
+              <span>{designer.name}</span>
+            </MProfile>
+          ))}
+        </MProfiles>
+      </MWrapper>
+    </Mobile>
+  </>
   );
 };
 export default Designer;
