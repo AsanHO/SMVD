fd042ae8ca91b7af7eee8bdb477ee72ad4c57b2a
youngsil <koo1175@naver.com>
2022-10-27 16:28:27 +0900

    useEffect

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { useEffect } from "react";
 
 const Overlay = styled.div`
   position: absolute;
